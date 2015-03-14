240a1c84e07d8b46922668e924fea7882682f9bd
Alexandre Couedelo <alexandre.couedelo@mines-ales.org>
2015-03-14 19:45:14 +0100

    gestion catégories de l'adhésion checker

--- src/emagine/SecGBundle/Controller/AdhesionCheckerController.php
+++ src/emagine/SecGBundle/Controller/AdhesionCheckerController.php
@@ -3,10 +3,16 @@
 namespace emagine\SecGBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use JMS\SecurityExtraBundle\Annotation\Secure;
+
 use emagine\SecGBundle\Entity\AdhesionCheckerCategory;
+use emagine\SecGBundle\Form\AdhesionCheckerCategoryType;
 
 class AdhesionCheckerController extends Controller
 {
+    /**
+     * @Secure(roles="ROLE_CA")
+    */
     public function indexAction()
     {
 
@@ -16,19 +22,79 @@ class AdhesionCheckerController extends Controller
         ));    
     }
 
+    /**
+     * @Secure(roles="ROLE_ADMIN")
+     */
     public function addCategoryAction()
     {
         $em = $this->getDoctrine()->getManager();
         $categories = $em->getRepository('emagineSecGBundle:AdhesionCheckerCategory')->findAll();
 
+        // On récupère le service
+        $security = $this->get('security.context');
+        // On récupère le token
+        $token = $security->getToken();
+        // on récupère l'utilisateur
+        $user = $token->getUser();
+
+        $category = new AdhesionCheckerCategory();
+
+        $form = $this->createForm(new AdhesionCheckerCategoryType(), $category);
+
+        if( $this->get('request')->getMethod() == 'POST' ){
+            $form->bind($this->get('request'));
+               
+            if( $form->isValid() ){
+
+                $em->persist($category);    
+                $em->flush();
+    
+                return $this->redirect( $this->generateUrl('emagine_secG_AdhesionChecker_show_category', array('id' => $category->getId())) );
+            }
+        }
+
         return $this->render('emagineSecGBundle:AdhesionChecker:addCategory.html.twig', array(
+            "form"       => $form->createView(),
             "categories" => $categories,
         )); 
    }
 
-   public function showCategory(){
-        return $this->render('emagineSecGBundle:AdhesionChecker:addCategory.html.twig', array(
-                // ...
+   /**
+    * @Secure(roles="ROLE_CA")
+   */
+   public function showCategoryAction($id){
+        $em = $this->getDoctrine()->getManager();
+        $category = $em->getRepository('emagineSecGBundle:AdhesionCheckerCategory')->find($id);
+
+        return $this->render('emagineSecGBundle:AdhesionChecker:showCategory.html.twig', array(
+                "category" => $category
+            ));    
+   }
+
+    /**
+    * @Secure(roles="ROLE_CA")
+   */
+   public function updateCategoryAction($id){
+        $em = $this->getDoctrine()->getManager();
+        $category = $em->getRepository('emagineSecGBundle:AdhesionCheckerCategory')->find($id);
+
+        $form = $this->createForm(new AdhesionCheckerCategoryType(), $category);
+
+        if( $this->get('request')->getMethod() == 'POST' ){
+            $form->bind($this->get('request'));
+               
+            if( $form->isValid() ){
+
+                $em->persist($category);    
+                $em->flush();
+    
+                return $this->redirect( $this->generateUrl('emagine_secG_AdhesionChecker_show_category', array('id' => $category->getId())) );
+            }
+        }
+
+        return $this->render('emagineSecGBundle:AdhesionChecker:updateCategory.html.twig', array(
+                "form"       => $form->createView(),
+                "category" => $category
             ));    
    }
 
--- src/mgate/PersonneBundle/Controller/PosteController.php
+++ src/mgate/PersonneBundle/Controller/PosteController.php
@@ -64,20 +64,18 @@ class PosteController extends Controller
     }
     
     /**
+     * Affiche la liste des pages et permet aux admin d'ajouter un poste
      * @Secure(roles="ROLE_SUIVEUR")
      */    
     public function indexAction($page)
     {
         $em = $this->getDoctrine()->getManager();
-
         $entities = $em->getRepository('mgatePersonneBundle:Poste')->findAll();
 
          // On récupère le service
         $security = $this->get('security.context');
-
         // On récupère le token
         $token = $security->getToken();
-
         // on récupère l'utilisateur
         $user = $token->getUser();
 
@@ -168,6 +166,7 @@ class PosteController extends Controller
             if( ! $entity = $em->getRepository('mgate\PersonneBundle\Entity\Poste')->find($id) )
                 throw $this->createNotFoundException('Le poste demandé n\'existe pas !');
             
+            var_dump($entity->getMembres());die();
             foreach($entity->getMembres() as $membre)
                     $membre->setPoste(null);
 
